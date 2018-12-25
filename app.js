296a9d672e3a2b3238f9b25867ce3e7473aa87b0
berianlowe <berian.lowe@gmail.com>
2018-12-25 22:17:53 +0000

    Stage2 Video 3 final

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const router = require('./router.js');
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;B
+const port = 3000;
 // Problem: We need a simple way to look at user's badge count and JavaScript points from a web browser.
 // Solution: Use Node.js to perform the profile look ups and server and serve our template via HTTP
 
--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+const Profile = require("./profile.js");
+
 // Handle HTTP route GET / and POST / i.e Home
 home = (request, response) => {
     // if url == "/" && GET
@@ -16,20 +18,35 @@ home = (request, response) => {
 user = (request, response) => {
     // if url == "/...."
     const username = request.url.replace("/", "");
-    if (username.length > 0){
+    if (username.length > 0) {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         response.write("Header\n");
-        response.write(username + "\n");
-        response.end("Footer\n");
-    }
-        // get json from Treehouse
-    // on "end"
-        // show profile
-    // on "error"
-        // show error
+        // get JSON from Treehouse
+        const studentProfile = new Profile(username);
+        //on end
+        studentProfile.on("end", function (profileJSON) {
+            // show Profile
+
+            //Store the values we need
+            const values = {
+                avatarURL: profileJSON.gravatar_url,
+                username: profileJSON.profile_name,
+                badges: profileJSON.badges.length,
+                javascriptPoints: profileJSON.points.JavaScript
+            }
+            //Simple response
+            response.write(`${values.username} has ${values.badges} badges.\n`);
+            response.end("Footer\n");
+        });
+        //on error
+        studentProfile.on("error", function (error) {
+            //show error
+            response.write(`${error.message} \n`);
+            response.end("Footer\n");
+        });
+    };
 }
 
 module.exports.home = home;
 module.exports.user = user;
-
